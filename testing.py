cd44aa33ce8bcbe155c56df5e8a9114cc545ef49
lov3catch <lov3catch@gmail.com>
2013-02-15 01:35:01 +0200

    test_v0.2

--- testing.py
+++ testing.py
@@ -11,10 +11,22 @@ for file in os.listdir(copy_from) :
 	print "-",file
 	if os.path.isdir(os.path.join(copy_from, file)):
 		temp_from = os.path.join(copy_from, file)
-		for file in os.listdir(temp_from):
-			print "--",file
-			if os.path.isdir(os.path.join(temp_from, file)):
-				temp_from = os.path.join(temp_from, file)
-				for file in os.listdir(temp_from):
-					print "---",file
+
+		#Запускаем цикл
+		while True :
+			for file in os.listdir(temp_from):
+				print "--",file
+				#Если выбраный объект - папка
+				if os.path.isdir(os.path.join(temp_from, file)):
+					temp_from = os.path.join(temp_from, file)
+				#Если выбраный объект - файл
+				if os.path.isfile(os.path.join(temp_from, file)):
+					print "file"
+			break
+
+
+
+
+					for file in os.listdir(temp_from):
+						print "---",file
 
