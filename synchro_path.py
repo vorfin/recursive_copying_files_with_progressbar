ef016ff96665898daaf21b1790f46db12428b87e
lov3catch <lov3catch@gmail.com>
2013-02-17 01:12:32 +0200

    STABLE v0.2

--- synchro_path.py
+++ synchro_path.py
@@ -1,31 +1,50 @@
-# -*- coding: utf-8; -*-
+#!/usr/bin/env python
+#-*- coding: utf-8; -*-
 
 import os
 import os.path
 import shutil
 
-my_path = os.path.dirname(__file__)
+copy_from = "/home/lov3catch/MyPyt/synchro_directory/copy_from_this_directory"
+copy_here = "/home/lov3catch/MyPyt/synchro_directory/copy_to_this_directory"
 
-copy_from = "D:/MyPyt/test_dir_2"
-copy_to = "D:/MyPyt/test_dir_3"
+dirs = [copy_from]
+files = []
 
-def copy_file (ctrl_c, ctrl_v, file_name):
-	print "COPY "+file_name+" to "+ctrl_v+"",
-	shutil.copy(ctrl_c, ctrl_v)
-	print " OK"
+def get_dirs(ctrl_c):
+	for files in os.listdir(ctrl_c):
+		obj_src = os.path.join(ctrl_c, files)
+		if os.path.isdir(obj_src):
+			dirs.append(obj_src)
 
-def make_dir (ctrl_v, file_name):
+def get_files(ctrl_c):
+	for same_obj in os.listdir(ctrl_c):
+		obj_src = os.path.join(ctrl_c, same_obj)
+		if os.path.isfile(obj_src):
+			files.append(obj_src)
+			print "File: ",obj_src
+
+def create_dirs(ctrl_c, ctrl_v):
+	relative_path = ctrl_c.replace(copy_from,"")
+	new_dir_src = ctrl_v+relative_path
+	print new_dir_src
 	try:
-		os.mkdir(os.path.join(ctrl_v, file_name))
-	except WindowsError:
-		print "ERROR"
+		os.mkdir(new_dir_src)
+	except OSError:
+		print "Folder already exist."
+	
+def copy_files(ctrl_c, ctrl_v):
+	#Генерируем путь для копирования файла. Удаляем имя файла из пути ctrl_c
+	ctrl_c_path = os.path.dirname(ctrl_c)
+	#Генерируем полный путь для копирования
+	ctrl_v = ctrl_v +"/"+os.path.relpath(ctrl_c ,copy_from)
+	print "ctrl_v: ", ctrl_v
+	shutil.copy(ctrl_c, ctrl_v)
 
-files_and_catalogs = os.listdir(copy_from)
-for file_name in files_and_catalogs :
-	absolute_path = os.path.join(copy_from, file_name)
-	#Копирование файлов
-	if os.path.isfile(absolute_path) :
-		copy_file(absolute_path, copy_to,file_name)
-	#Создание папок
-	if os.path.isdir(absolute_path) :
-		make_dir(copy_to, file_name)
+for element in dirs:
+	get_dirs(element)
+	get_files(element)
+	create_dirs(element, copy_here)
+
+for element in files:
+	copy_files(element, copy_here)
